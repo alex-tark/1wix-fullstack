ccb635f98237457413a84cd400c001515246bdb3
Alexander Zinin <azinin@dengabank.ru>
2018-06-21 19:16:09 +0300

    Server side Реализован базовый http сервер на Koa2 Нстроено подключение к БД Добавлены необходимые DAO Настроен скелет роутера под архитектуру vue.js клиента Написаны осовные запросы к БД для модели книг

---
+++ server/entities/Book.js
@@ -0,0 +1,52 @@
+import db from '../db'
+
+const Book = {}
+
+Book.get = async ({ field = 'id', sort = 'asc', page = 1, count = 10, search = '' }) => {
+    let sqlBooks = 'SELECT `id`, `title`, `author`, `description`, `date` FROM `books`'
+    let sqlTotal = 'SELECT COUNT(`id`) as total FROM `books`'
+
+    let values = [field, count, ((page - 1) * count)]
+    const searchArray = new Array(3)
+
+    if (search) {
+        searchArray.fill(`%${search}%`)
+        values = [...searchArray, ...values]
+        sqlBooks += ' WHERE `title` LIKE ? OR `author` LIKE ? OR `description` LIKE ?'
+        sqlTotal += ' WHERE `title` LIKE ? OR `author` LIKE ? OR `description` LIKE ?'
+    }
+
+    sqlBooks += ' ORDER BY ??'
+
+    if (sort === 'desc') {
+        sqlBooks += ' DESC'
+    }
+
+    sqlBooks += ' LIMIT ? OFFSET ?'
+
+    const books = await db.query(sqlBooks, values)
+    const total = await db.query(sqlTotal, values)
+    return { books, total: total[0].total }
+}
+
+Book.getById = async (id) => {
+    const sql = 'SELECT `id`, `title`, `author`, `description`, `date` FROM `books` WHERE ?'
+    return db.query(sql, id)
+}
+
+Book.create = async (payload) => {
+    const sql = 'INSERT INTO `books` SET ?'
+    return db.query(sql, payload)
+}
+
+Book.update = async (payload, id) => {
+    const sql = 'UPDATE `books` SET ? WHERE id= ?'
+    return db.query(sql, [payload, id])
+}
+
+Book.destroy = async (id) => {
+    const sql = 'DELETE FROM `books` WHERE id=?'
+    return db.query(sql, [id])
+}
+
+export default Book
---
+++ server/seeds/fill-data.js
@@ -0,0 +1,23 @@
+const faker = require('faker')
+const moment = require('moment')
+
+function generateBooks() {
+    const data = []
+    for (let i = 0; i < 100000; i += 1) {
+        const book = {}
+        book.title = faker.company.bs()
+        book.author = faker.fake('{{name.lastName}} {{name.firstName}}')
+        book.description = faker.hacker.phrase()
+        book.date = faker.date.past()
+        book.date = moment(book.date).format('YYYY-MM-DD')
+
+        data.push(book)
+    }
+
+    return data
+}
+
+exports.seed = (knex) => {
+    const books = generateBooks()
+    return knex.table('books').insert(books)
+}
